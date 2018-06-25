22c22ce625293fedeb9b675526cfd5465e57bf11
Muhammad Fadhlan Supriadi <fadhlansupriadi@gmail.com>
2018-06-25 11:19:46 +0800

    Initial commit

---
+++ app/src/main/java/com/telkom/ccan/beritaacarapenyelesaiangangguanccan/InitialPageActivity.java
@@ -0,0 +1,13 @@
+package com.telkom.ccan.beritaacarapenyelesaiangangguanccan;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class InitialPageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_initial_page);
+    }
+}
